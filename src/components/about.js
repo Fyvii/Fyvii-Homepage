ff8b5a6400d0e93db58110a46d816893e3030f9c
Nuur Kahalah <nuur.dev@outlook.com>
2022-04-25 15:59:05 -0600

    about 2.0

--- src/components/about.js
+++ src/components/about.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import {
-  Box,
+  Container,
   Text,
   VStack,
   Heading,
   Image,
   HStack,
   Center,
+  Box,
 } from '@chakra-ui/react';
 
 function About() {
   return (
-    <Center>
-      <VStack>
-        <Box>
-          <HStack>
-            <Heading as="h1" size="lg" color="">
-              Noor Kahalah
-            </Heading>
-            <Image
-              boxSize="100px"
-              borderRadius="full"
-              objectFit="cover"
-              src=" https://i.pinimg.com/564x/e5/2a/51/e52a512d1eee143e47ffd1b411bc523c.jpg"
-              alt="nuur"
-            ></Image>
-          </HStack>
-        </Box>
-        <Text fontSize="md">
-          Welcome to my personal portfolio! Here you can check out more about
-          myself and career!
-        </Text>
-        <Text color="gray.500" fontSize="md">
-          Check out my latest projects!
-        </Text>
-      </VStack>
-    </Center>
+    <Container maxW="sm" p="10px">
+      <Center>
+        <VStack>
+          <Box>
+            <HStack>
+              <Heading as="h1" size="lg" color="">
+                Noor Kahalah
+              </Heading>
+              <Image
+                boxSize="100px"
+                borderRadius="full"
+                objectFit="cover"
+                src="https://i.pinimg.com/564x/93/e9/b2/93e9b2c063be5cea37ef6f751a68935c.jpg"
+                alt="nuur"
+              ></Image>
+            </HStack>
+          </Box>
+          <Text fontSize="md">
+            Welcome to my personal portfolio! Here you can check out more about
+            myself and career!
+          </Text>
+          <Text color="gray.500" fontSize="md" paddingTop="5px">
+            Check out my latest projects!
+          </Text>
+        </VStack>
+      </Center>
+    </Container>
   );
 }
 
